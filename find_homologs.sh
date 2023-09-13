3f1277e5588b7444df06fcd23dbf7774ec86e0f2
Tarek Hachad <hachadtarek@gmail.com>
2023-09-12 21:11:16 -0400

    Bug: replaced blastn-short with tblastn after -task

--- find_homologs.sh
+++ find_homologs.sh
@@ -2,7 +2,7 @@
 
 # Run BLAST and personalize format to include necessary information
 
-tblastn -query "$1" -subject "$2" -outfmt '6 pident length qlen' -task blastn-short | awk '$1 > 30.000 && $2 > $3' > $3
+tblastn -query "$1" -subject "$2" -outfmt '6 pident length qlen' -task tblastn | awk '$1 > 30.000 && $2 > $3' > $3
 
 # Print number of perfect matches to stdout
 

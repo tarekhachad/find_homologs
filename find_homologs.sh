41e08243f15dfcf771e9681d27bd860d4d8313c6
Tarek Hachad <hachadtarek@gmail.com>
2023-09-12 20:50:14 -0400

    Change to following selected blast columns only: pident, length, qlen

--- find_homologs.sh
+++ find_homologs.sh
@@ -2,7 +2,7 @@
 
 # Run BLAST and personalize format to include necessary information
 
-tblastn -query "$1" -subject "$2" -outfmt '6 qseqid sseqid pident qlen slen' -task blastn-short | awk '$3 == 100.00 && $4 == $5' > $3
+tblastn -query "$1" -subject "$2" -outfmt '6 pident length qlen' -task blastn-short | awk '$3 == 100.00 && $4 == $5' > $3
 
 # Print number of perfect matches to stdout
 

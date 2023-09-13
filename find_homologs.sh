5e1536c7de33988086fd196fcc4898b660509b74
Tarek Hachad <hachadtarek@gmail.com>
2023-09-12 20:43:46 -0400

    Changed BLAST program blatn -> tblastn
    tblastn is used for protein query and nucleotide subject

--- find_homologs.sh
+++ find_homologs.sh
@@ -2,12 +2,9 @@
 
 # Run BLAST and personalize format to include necessary information
 
-blastn -query "$1" -subject "$2" -outfmt '6 qseqid sseqid pident qlen slen' -task blastn-short | awk '$3 == 100.00 && $4 == $5' > $3
+tblastn -query "$1" -subject "$2" -outfmt '6 qseqid sseqid pident qlen slen' -task blastn-short | awk '$3 == 100.00 && $4 == $5' > $3
 
 # Print number of perfect matches to stdout
 
 cat "$3" | wc -l
 
-# remove temp output file
-
-rm temp_output.txt
